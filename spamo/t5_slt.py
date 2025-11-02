c4f3268b7f37cdd879b7f5ca2f0ab0cb6f9f11e2
mohassan5286 <mohassan5286@gmail.com>
2025-11-02 13:37:43 +0200

    fix a bug

--- spamo/t5_slt.py
+++ spamo/t5_slt.py
@@ -58,6 +58,7 @@ class FlanT5SLT(AbstractSLT):
         **kwargs
     ):
         super().__init__(**kwargs)
+        self.save_hyperparameters()
         
         # Configuration parameters
         self.input_size = input_size
